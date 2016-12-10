7b335e2b4fcc660c4e6c4b7b3cd2d190e6ac008e
Michela Paganini <micky.91@hotmail.com>
2016-12-10 03:28:49 -0800

    Micky's comments and small edits (normal noise, part of data, ...)

--- models/acgan_jets.py
+++ models/acgan_jets.py
@@ -39,19 +39,19 @@ def build_generator(latent_size):
 
     # upsample to (..., 64, 14, 14)
     cnn.add(UpSampling2D(size=(2, 2)))
-    cnn.add(Convolution2D(64, 5, 5, border_mode='same', init='glorot_normal'))
+    cnn.add(Convolution2D(256, 5, 5, border_mode='same', init='glorot_normal'))
     cnn.add(LeakyReLU())
 
     # upsample to (..., 64, 28, 28)
     cnn.add(UpSampling2D(size=(2, 2)))
 
     # valid conv to (..., 32, 25, 25)
-    cnn.add(Convolution2D(32, 4, 4, border_mode='valid', init='glorot_normal'))
+    cnn.add(Convolution2D(128, 4, 4, border_mode='valid', init='glorot_normal'))
     cnn.add(LeakyReLU())
 
     # take a channel axis reduction to (..., 1, 25, 25)
     cnn.add(Convolution2D(1, 2, 2, border_mode='same', init='glorot_normal'))
-    cnn.add(Activation('relu'))
+#    cnn.add(Activation('relu'))
 
     # this is the z space commonly refered to in GAN papers
     latent = Input(shape=(latent_size, ))
@@ -84,11 +84,11 @@ def build_discriminator():
     cnn.add(LeakyReLU())
     cnn.add(Dropout(0.3))
 
-    cnn.add(Convolution2D(64, 3, 3, border_mode='same', subsample=(2, 2)))
+    cnn.add(Convolution2D(128, 3, 3, border_mode='same', subsample=(2, 2)))
     cnn.add(LeakyReLU())
     cnn.add(Dropout(0.3))
 
-    cnn.add(Convolution2D(64, 3, 3, border_mode='same', subsample=(2, 2)))
+    cnn.add(Convolution2D(256, 3, 3, border_mode='same', subsample=(2, 2)))
     cnn.add(LeakyReLU())
     cnn.add(Dropout(0.3))
 
@@ -124,23 +124,26 @@ if __name__ == '__main__':
     image_class = Input(shape=(1, 1), dtype='int32')
 
     # get a fake image
-    fake = generator([latent, image_class])
+    fake_image = generator([latent, image_class])
 
     # we only want to be able to train generation for the combined model
     discriminator.trainable = False
-    fake, aux = discriminator(fake)
-    combined = Model(input=[latent, image_class], output=[fake, aux])
+    fake_bool, aux = discriminator(fake_image)
+    combined = Model(input=[latent, image_class], output=[fake_bool, aux])
 
     combined.compile(adam_config(), ['binary_crossentropy',
                                      'binary_crossentropy'])
 
-    discriminator.trainable = True
+    #discriminator.trainable = True
 
     # get our mnist data, and force it to be of shape (..., 1, 28, 28) with
     # range [-1, 1]
 
-    d = np.load('/home/lukedeo/scratch/data/gan/jetimages.npy')
-    X = d['image']
+    d = np.load('/home/lukedeo/scratch/data/gan/jetimages.npy', mmap_mode='r')
+    ix = range(d.shape[0])
+    np.random.shuffle(ix)
+    d = np.array(d[ix][:90000])
+    X = d['image'] / 120.0
     y = d['signal']
 
     from sklearn.cross_validation import train_test_split
@@ -161,70 +164,79 @@ if __name__ == '__main__':
         # for index in tqdm(range(nb_batches), unit=' batches'):
         for index in xrange(nb_batches):
             progress_bar.update(index)
+            # ------------------------ 
+            # JUST GENERATE FAKE IMAGES WITH CURRENT STATE OF G
             # generate a new batch of noise
-            noise = np.random.uniform(-1, 1, (batch_size, latent_size))
+            #noise = np.random.uniform(-1, 1, (batch_size, latent_size))
+            noise = np.random.normal(0, 1, (batch_size, latent_size))
 
             # get a batch of real images
             image_batch = X_train[index * batch_size:(index + 1) * batch_size]
             label_batch = y_train[index * batch_size:(index + 1) * batch_size]
 
-            # sample some labels from p_c
+            # requested labels of fake images
             sampled_labels = np.random.randint(0, nb_labels, batch_size)
 
-            # ...and a batch of fake images, using the generated labels as a
+            # generate a batch of fake images, using the requested labels as a
             # conditioner
             generated_images = generator.predict(
                 [noise, sampled_labels.reshape((-1, 1, 1))], verbose=0)
 
+            # ------------------------
+            # TRAIN D TO DISCRIMINATE FAKE FROM REAL IMAGES
+            # -- once you generated images from noise, concatenate them with 
+            # -- the real ones to then pass them to the D
             X = np.concatenate((image_batch, generated_images))
             y = np.array([1] * batch_size + [0] * batch_size)
             aux_y = np.concatenate((label_batch, sampled_labels), axis=0)
 
-            # see if the discriminator can figure itself out...
+            # this is like .fit(), and it outputs a loss per batch
             epoch_disc_loss.append(discriminator.train_on_batch(X, [y, aux_y]))
 
+            # -----------------------
+            # TRAIN THE G TO TRICK THE D
             # make new noise
-            noise = np.random.uniform(-1, 1, (2 * batch_size, latent_size))
+            # noise = np.random.uniform(-1, 1, (2 * batch_size, latent_size))
+            noise = np.random.normal(0, 1, (2 * batch_size, latent_size))
             sampled_labels = np.random.randint(0, nb_labels, 2 * batch_size)
 
-            discriminator.trainable = False
-
-            trick = np.ones(2 * batch_size)
+            trick = np.ones(2 * batch_size) # G wants D to say 1 for all fake images
 
             epoch_gen_loss.append(combined.train_on_batch(
                 [noise, sampled_labels.reshape((-1, 1, 1))], [trick, sampled_labels]))
 
-            discriminator.trainable = True
 
         print "\nTesting for epoch {}:".format(epoch + 1)
 
         # evaluate the testing loss
 
         # generate a new batch of noise
-        noise = np.random.uniform(-1, 1, (nb_test, latent_size))
-
+        #noise = np.random.uniform(-1, 1, (nb_test, latent_size))
+        noise = np.random.normal(0, 1, (nb_test, latent_size))
+        
         # get a batch of real images
-        image_batch = X_train
-        label_batch = y_train
+        image_batch = X_test
+        label_batch = y_test
 
-        # sample some labels from p_c
+        # request labels for generated images
         sampled_labels = np.random.randint(0, nb_labels, nb_test)
 
-        # ...and a batch of fake images, using the generated labels as a
+        # generate a batch of fake images, using the requested labels as a
         # conditioner
         generated_images = generator.predict(
             [noise, sampled_labels.reshape((-1, 1, 1))], verbose=False)
 
         X = np.concatenate((image_batch, generated_images))
-        y = np.array([1] * nb_train + [0] * nb_test)
+        y = np.array([1] * nb_test + [0] * nb_test)
         aux_y = np.concatenate((label_batch, sampled_labels), axis=0)
 
-        # see if the discriminator can figure itself out...
+        # evaluate the performance of D in terms of its loss
         discriminator_test_loss = discriminator.evaluate(
             X, [y, aux_y], verbose=False)
 
         # make new noise
-        noise = np.random.uniform(-1, 1, (2 * nb_test, latent_size))
+        #noise = np.random.uniform(-1, 1, (2 * nb_test, latent_size))
+        noise = np.random.normal(0, 1, (2 * nb_test, latent_size))
         sampled_labels = np.random.randint(0, nb_labels, 2 * nb_test)
 
         trick = np.ones(2 * nb_test)
