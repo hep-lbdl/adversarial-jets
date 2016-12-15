f5788e13e0b459522f3b6652592e4c7a78e62988
Michela Paganini <micky.91@hotmail.com>
2016-12-15 06:31:25 -0800

    Express hyperbolic functions in terms of exp, fix elif bug

--- models/mass.py
+++ models/mass.py
@@ -24,22 +24,29 @@ def image_mass_func(eta_grid=None, phi_grid=None, shape=(None, 1, 25, 25), use_k
     if eta_grid is None and phi_grid is None:
         eta_grid = np.tile(np.linspace(-1.2, 1.2, 25), (25, 1))
         phi_grid = np.tile(np.linspace(-1.2, 1.2, 25).reshape(-1, 1), (1, 25))
-
+        
     if len(shape) > 2:
         axis_slice = tuple(range(1, len(shape[1:]) + 1))
-    if len(shape) == 2:
+    elif len(shape) == 2:
         axis_slice = None
     else:
         raise ValueError('invalid shape')
+        
+    def sinh(x):
+        return (K.exp(x) - K.exp(-x)) / 2
+    
+    def cosh(x):
+        return (K.exp(x) + K.exp(-x)) / 2
 
     def _(jet_image):
         Px = K.sum(jet_image * K.cos(phi_grid), axis=axis_slice)
         Py = K.sum(jet_image * K.sin(phi_grid), axis=axis_slice)
 
-        Pz = K.sum(jet_image * K.sinh(eta_grid), axis=axis_slice)
-        E = K.sum(jet_image * K.cosh(eta_grid), axis=axis_slice)
+        Pz = K.sum(jet_image * sinh(eta_grid), axis=axis_slice)
+        E = K.sum(jet_image * cosh(eta_grid), axis=axis_slice)
 
         PT = K.sqrt(K.square(Px) + K.square(Py))
         M2 = K.square(E) - (K.square(PT) + K.square(Pz))
         M = K.sqrt(M2)
+        return M
     return _
